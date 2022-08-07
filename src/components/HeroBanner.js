4488181b988097ee409563b6bf0378de56ab9fbf
Mike <mikejloria@gmail.com>
2022-08-07 14:27:08 -0700

    hero banner now complete

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -7,8 +7,7 @@ const HeroBanner = () => {
   return (
     <Box sx={{
       mt: { lg: '212px', xs: '70px' },
-      ml: { sm: '50px' }
-    }} position="relative" p="20px">
+      ml: { sm: '50px' }}} position="relative" p="20px">
       <Typography color="#FF2625" fontWeight="600" fontSize="26px">
         Fitness Club
       </Typography>
